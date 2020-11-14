507b730abc2d6486d5c0f21bdc5db4379c995d1f
Yeolnim <49664111+Yeolnim@users.noreply.github.com>
2020-11-14 15:53:16 +0800

    Add files via upload

---
+++ perplexity.py
@@ -0,0 +1,94 @@
+from gensim import corpora, models
+
+
+def ldamodel(num_topics):
+    cop = open(r'output/data.txt', 'r', encoding='UTF-8')
+
+    train = []
+    for line in cop.readlines():
+        line = [word.strip() for word in line.split(' ')]
+        train.append(line)  # list of list 格式
+
+    dictionary = corpora.Dictionary(train)
+    corpus = [dictionary.doc2bow(text) for text in
+              train]  # corpus里面的存储格式（0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1)
+    corpora.MmCorpus.serialize('corpus.mm', corpus)
+    lda = models.LdaModel(corpus=corpus, id2word=dictionary, random_state=1,
+                          num_topics=num_topics)  # random_state 等价于随机种子的random.seed()，使每次产生的主题一致
+
+    topic_list = lda.print_topics(num_topics, 5)
+    # print("主题的单词分布为：\n")
+    # for topic in topic_list:
+    #     print(topic)
+    return lda, dictionary
+
+import math
+
+def perplexity(ldamodel, testset, dictionary, size_dictionary, num_topics):
+    print('the info of this ldamodel: \n')
+    print('num of topics: %s' % num_topics)
+    prep = 0.0
+    prob_doc_sum = 0.0
+    topic_word_list = []
+    for topic_id in range(num_topics):
+        topic_word = ldamodel.show_topic(topic_id, size_dictionary)
+        dic = {}
+        for word, probability in topic_word:
+            dic[word] = probability
+        topic_word_list.append(dic)
+    doc_topics_ist = []
+    for doc in testset:
+        doc_topics_ist.append(ldamodel.get_document_topics(doc, minimum_probability=0))
+    testset_word_num = 0
+    for i in range(len(testset)):
+        prob_doc = 0.0  # the probablity of the doc
+        doc = testset[i]
+        doc_word_num = 0
+        for word_id, num in dict(doc).items():
+            prob_word = 0.0
+            doc_word_num += num
+            word = dictionary[word_id]
+            for topic_id in range(num_topics):
+                # cal p(w) : p(w) = sumz(p(z)*p(w|z))
+                prob_topic = doc_topics_ist[i][topic_id][1]
+                prob_topic_word = topic_word_list[topic_id][word]
+                prob_word += prob_topic * prob_topic_word
+            prob_doc += math.log(prob_word)  # p(d) = sum(log(p(w)))
+        prob_doc_sum += prob_doc
+        testset_word_num += doc_word_num
+    prep = math.exp(-prob_doc_sum / testset_word_num)  # perplexity = exp(-sum(p(d)/sum(Nd))
+    print("模型困惑度的值为 : %s" % prep)
+    return prep
+
+
+from gensim import corpora, models
+import matplotlib.pyplot as plt
+# import perplexity
+# import lda_catch
+
+
+def graph_draw(topic, perplexity):  # 做主题数与困惑度的折线图
+    x = topic
+    y = perplexity
+    plt.plot(x, y, color="red", linewidth=2)
+    plt.xlabel("Number of Topic")
+    plt.ylabel("Perplexity")
+    plt.show()
+
+
+if __name__ == '__main__':
+    for i in range(20, 300, 1):  # 多少文档中抽取一篇（这里只是为了调试最优结果，可以直接设定不循环）
+        print("抽样为" + str(i) + "时的perplexity")
+        a = range(1, 20, 1)  # 主题个数
+        p = []
+        for num_topics in a:
+
+            lda, dictionary = ldamodel(num_topics)
+            corpus = corpora.MmCorpus('corpus.mm')
+            testset = []
+            for c in range(int(corpus.num_docs / i)):
+                testset.append(corpus[c * i])
+            prep = perplexity(lda, testset, dictionary, len(dictionary.keys()), num_topics)
+            p.append(prep)
+
+        graph_draw(a, p)
---
+++ topic_extraction.py
@@ -0,0 +1,91 @@
+import jieba, os
+from gensim import corpora, models, similarities
+import re
+
+"""创建停用词列表"""
+def stopwordslist():
+    stopwords = [line.strip() for line in open('./stopwords.txt', encoding='UTF-8').readlines()]
+    return stopwords
+
+"""对句子进行中文分词"""
+def seg_depart(sentence):
+    sentence_depart = jieba.cut(sentence.strip())
+    stopwords = stopwordslist()
+    outstr = ''
+    for word in sentence_depart:
+        if word not in stopwords:
+            outstr += word
+            outstr += " "
+    return outstr
+
+"""如果文档还没分词，就进行分词"""
+if not os.path.exists('output/data_jieba.txt'):
+    # 给出文档路径
+    filename = "output/data.txt"
+    outfilename = "output/data_jieba.txt"
+    inputs = open(filename, 'r', encoding='UTF-8')
+    outputs = open(outfilename, 'w', encoding='UTF-8')
+
+    # 把非汉字的字符全部去掉
+    # 将输出结果写入ouputs.txt中
+    for line in inputs:
+        print(line)
+        #line = line.split('\t')[1]
+        line = re.sub(r'[^\u4e00-\u9fa5]+', '', line)
+        line_seg = seg_depart(line.strip())
+        outputs.write(line_seg.strip() + '\n')
+
+    outputs.close()
+    inputs.close()
+    print("删除停用词和分词成功！！！")
+
+"""准备好训练语料，整理成gensim需要的输入格式"""
+fr = open('output/data_jieba.txt', 'r', encoding='utf-8')
+train = []
+for line in fr.readlines():
+    line = [word.strip() for word in line.split(' ')]
+    train.append(line)
+
+"""构建词频矩阵，训练LDA模型"""
+dictionary = corpora.Dictionary(train)
+# corpus[0]: [(0, 1), (1, 1), (2, 1), (3, 1), (4, 1),...]
+# corpus是把每条专利ID化后的结果，每个元素是专利中的每个词语，在字典中的ID和频率
+corpus = [dictionary.doc2bow(text) for text in train]
+
+print("词典中单词个数：",len(dictionary))
+lda = models.LdaModel(corpus=corpus, id2word=dictionary, num_topics=5,)
+#k越多，训练越复杂，但是，如果数据量不多的情况下，很难提取出小样本数据的主题
+#所以主题挖掘的应用到底在哪？
+topic_list = lda.print_topics(8)
+
+print("\n5个主题的单词分布为：\n")
+for topic in topic_list:
+    print(topic)
+lda.save("lda.model")
+"""抽取专利的主题"""
+def Topic(file_test = "output/test.txt"):
+    # 用来测试的专利
+    news_test = open(file_test, 'r', encoding='UTF-8')
+
+    test = []
+    count = 0
+    # 处理成正确的输入格式
+    for line in news_test:
+        # line = line.split('\t')[1]
+        line = re.sub(r'[^\u4e00-\u9fa5]+', '', line)
+        line_seg = seg_depart(line.strip())
+        line_seg = [word.strip() for word in line_seg.split(' ')]
+        test.append(line_seg)
+        count += 1
+
+    # 专利ID化
+    corpus_test = [dictionary.doc2bow(text) for text in test]
+    # 得到每条专利的主题分布
+    topics_test = lda.get_document_topics(corpus_test)
+
+    for i in range(count):
+        print(str(i + 1) + ":", topics_test[i], '\n')
+    fr.close()
+    news_test.close()
+
+Topic()
